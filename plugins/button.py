3ddd544fc8cdc79ff11003f14ede593dd15d4404
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-06-26 11:48:15 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -116,6 +116,7 @@ async def youtube_dl_call_back(bot, update):
             "yt-dlp",
             "-c",
             "--geo-bypass",
+            "--recode-video", "mp4", "-k",
             "--clean-info-json",
             "--ignore-no-formats-error",
             "--embed-subs",
