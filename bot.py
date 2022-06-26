945f1bf3fbebee04fdfa48c3b9c09b52d61c3714
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-06-26 08:44:20 +0530

    Update bot.py

--- bot.py
+++ bot.py
@@ -9,7 +9,7 @@ logger = logging.getLogger(__name__)
 import os
 from plugins.config import Config
 
-from pyrogram import Client as 5uperB0t
+from pyrogram import Client as sprbt
 from pyrogram import filters
 logging.getLogger("pyrogram").setLevel(logging.WARNING)
 
@@ -19,10 +19,10 @@ if __name__ == "__main__" :
     if not os.path.isdir(Config.DOWNLOAD_LOCATION):
         os.makedirs(Config.DOWNLOAD_LOCATION)
     plugins = dict(root="plugins")
-    5uperB0t = 5uperB0ts(
+    sprbts = sprbt(
         "Uploader Bot",
         bot_token=Config.BOT_TOKEN,
         api_id=Config.API_ID,
         api_hash=Config.API_HASH,
         plugins=plugins)
-    5uperB0t.run()
+    sprbts.run()
