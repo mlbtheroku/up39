20c33ae2d508d543e0f25fd00d2f842c6b7bf87e
cvatricks <68840422+cvatricks@users.noreply.github.com>
2022-06-26 08:19:22 +0530

    Update button.py

--- plugins/button.py
+++ plugins/button.py
@@ -202,6 +202,16 @@ async def youtube_dl_call_back(bot, update):
             parse_mode=enums.ParseMode.HTML,
             text=error_message
         )
+        Config.ONE_BY_ONE.remove(update.from_user.id)
+        total_req_get = total_req
+        b_json["users"].pop(user_count - 1)
+        b_json["users"].append({
+             "user_id": "{}".format(update.from_user.id),
+             "total_req": "{}".format(int(total_req_get)),
+             "exp_req": "{}".format(datetime.now())
+        })
+        with open("backup.json", "w", encoding="utf8") as outfile:
+              json.dump(b_json, outfile, ensure_ascii=False)
         return False
 
     if t_response:
@@ -337,4 +347,13 @@ async def youtube_dl_call_back(bot, update):
                 caption=Translation.AFTER_SUCCESSFUL_UPLOAD_MSG_WITH_TS.format(time_taken_for_download, time_taken_for_upload),
                 parse_mode=enums.ParseMode.HTML
             )
-
+            Config.ONE_BY_ONE.remove(update.from_user.id)
+            total_req_get = total_req
+            b_json["users"].pop(user_count - 1)
+            b_json["users"].append({
+                 "user_id": "{}".format(update.from_user.id),
+                 "total_req": "{}".format(int(total_req_get)),
+                 "exp_req": "{}".format(datetime.now())
+            })
+            with open("backup.json", "w", encoding="utf8") as outfile:
+                  json.dump(b_json, outfile, ensure_ascii=False)
